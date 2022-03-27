b86d95909c6929cad580e2ddc91efe21c1f5b321
Rama Mohan Vishnu Guturu <rm286720@dal.ca>
2022-03-27 20:40:34 -0300

    User interface module code changes

--- src/main/java/userinterface/Menu.java
+++ src/main/java/userinterface/Menu.java
@@ -17,12 +17,14 @@ public class Menu
             case 1:
             {
                 Registration.registerUser();
+                mainMenu();
                 break;
             }
 
             case 2:
             {
                 Login.loginUser();
+                mainMenu();
                 break;
             }
 
@@ -35,19 +37,54 @@ public class Menu
             default:
             {
                 System.out.println("You entered an invalid input. Please choose from one of the below options!");
-                Menu.mainMenu();
+                mainMenu();
             }
         }
     }
 
-    public static int loginMenu(String username)
+    public static void loginMenu(String username)
     {
-        System.out.println("Please choose one of the following options:");
+        System.out.println("\nPlease choose one of the following options:");
         System.out.println("1. WRITE QUERIES");
         System.out.println("2. EXPORT");
         System.out.println("3. DATA MODEL");
-        System.out.println("3. ANALYTICS");
+        System.out.println("4. ANALYTICS");
+        System.out.println("5. SHOW LOGS");
         Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
+        int option = sc.nextInt();
+
+        switch (option)
+        {
+            case 1:
+            {
+
+            }
+
+            case 2:
+            {
+
+            }
+
+            case 3:
+            {
+
+            }
+
+            case 4:
+            {
+
+            }
+
+            case 5:
+            {
+
+            }
+
+            default:
+            {
+                System.out.println("You entered an invalid input. Please choose from one of the below options!");
+                loginMenu(username);
+            }
+        }
     }
 }

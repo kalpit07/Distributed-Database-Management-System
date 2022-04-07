b6594a712ceb90773c8fa5ec7d367556a8b0b198
kishan <ks317715@dal.ca>
2022-04-07 12:07:08 -0300

    Transaction updated.

---
+++ src/main/java/Transaction/Config.java
@@ -0,0 +1,4 @@
+public class Config {
+    public static String[] DDL = new String[]{"select","create"};
+    public static String[] DML = new String[]{"insert","update","delete"};
+}
---
+++ src/main/java/Transaction/Transaction.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+/**
+ * @author : Kishan Savaliya
+ */
+public class Transaction {
+
+    //For storing query validation and type this kind:--- true;DDL
+    public static HashMap<String, String> queryTable = new HashMap<String,String>();
+
+
+    //Taking input in transaction module
+    public static void takeInputQuery(){
+        boolean flag = true;
+        Scanner sc = new Scanner(System.in);
+        while(flag){
+            System.out.println("Enter Query:");
+            String query = sc.nextLine();
+            if(query.equalsIgnoreCase("commit;")){
+                break;
+            }
+            makeHashmap(query);
+        }
+    }
+
+    public static void makeHashmap(String query){
+
+        boolean validation = true;
+        String[] words = query.split(" ");
+        if(Arrays.asList(Config.DDL).contains(words[0].toLowerCase())){
+            if(validation){
+                queryTable.put(query,"true;DDL");
+            } else {
+                queryTable.put(query,"false;DDL");
+            }
+        } else if(Arrays.asList(Config.DML).contains(words[0].toLowerCase())){
+            if(validation){
+                queryTable.put(query,"true;DML");
+            } else {
+                queryTable.put(query,"false;DML");
+            }
+        } else {
+            //nothing
+        }
+        System.out.println(queryTable.get(query));
+
+    }
+
+    public static void main(String[] args){
+        takeInputQuery();
+        startTransaction();
+    }
+
+    public static void startTransaction(){
+
+        String[] keys = queryTable.keySet().toArray(new String[0]);
+
+        //For seperating DLL queries
+        ArrayList<String> DDL_query = new ArrayList<String>();
+        boolean flag = false;
+        for(String key : keys){
+            String value = queryTable.get(key);
+            String[] results = value.split(";");
+            if(results[1].equalsIgnoreCase("ddl")){
+                DDL_query.add(key);
+            }
+            if(results[0].equalsIgnoreCase("false")){
+                flag = true;
+                break;
+            }
+        }
+        System.out.println(DDL_query);
+        if(!flag){
+            System.out.println("All queries are valid.");
+        }
+    }
+}

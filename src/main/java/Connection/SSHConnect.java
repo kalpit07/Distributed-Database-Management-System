244999f227ad1e0030dcb522e841c076d9f119e7
sharad <sh244392.dal.ca>
2022-04-07 12:44:04 -0300

    tested connection with gcp

---
+++ src/main/java/Connection/SSHConnect.java
@@ -0,0 +1,53 @@
+package Connection;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+import java.io.ByteArrayOutputStream;
+
+public class SSHConnect {
+
+    public static void main(String []args) throws Exception {
+        listFolderStructure("gcpKey", "", "34.136.127.213", 22, "dir");
+    }
+
+    public static void listFolderStructure(String username, String password,
+                                           String host, int port, String command) throws Exception {
+
+        JSch jsch=null;
+        Session session = null;
+        ChannelExec channel = null;
+
+        try {
+            jsch=new JSch();
+            jsch.setKnownHosts("C:\\Users\\shara\\.ssh/known_hosts");
+            jsch.addIdentity("C:\\Users\\shara\\.ssh/id_rsa");
+            session = jsch.getSession(username, host, port);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+
+            session.connect();
+
+            channel = (ChannelExec) session.openChannel("exec");
+            channel.setCommand(command);
+            ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+            channel.setOutputStream(responseStream);
+            channel.connect();
+
+            while (channel.isConnected()) {
+                Thread.sleep(100);
+            }
+
+            String responseString = new String(responseStream.toByteArray());
+            System.out.println(responseString);
+        } finally {
+            if (session != null) {
+                session.disconnect();
+            }
+            if (channel != null) {
+                channel.disconnect();
+            }
+        }
+    }
+}

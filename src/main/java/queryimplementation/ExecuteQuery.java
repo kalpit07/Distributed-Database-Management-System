edd99eba65bc6205dd1d845e9c384414ac5cec4f
Kalpit Machhi <kl567059@dal.ca>
2022-04-07 21:10:21 -0300

    Query execution

--- src/main/java/queryimplementation/ExecuteQuery.java
+++ src/main/java/queryimplementation/ExecuteQuery.java
@@ -1,74 +1,398 @@
 package queryimplementation;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.*;
 
 import static queryimplementation.ParseQuery.*;
 import static queryimplementation.QueryImplementation.*;
 
 public class ExecuteQuery {
-    public static void executeCreate(String query) throws IOException {
+
+    public static void executeCreateDatabase(String query) throws IOException {
         String[] query_parts = query.split("\\s+");
         String database = query_parts[2];
 
-//        new File(BASE_DIRECTORY + database).mkdirs();
-//        FileWriter fw_global = new FileWriter(BASE_DIRECTORY + GLOBAL_METADATA_FILE, true);
-//        FileWriter fw_local = new FileWriter(BASE_DIRECTORY + LOCAL_METADATA_FILE, true);
+        new File(BASE_DIRECTORY + database).mkdirs();
 
-//        fw_global.write(database + "|" + "local\n");
-//        fw_local.write(database + "\n");
-//
-//        fw_global.close();
-//        fw_local.close();
+        String meta_data_file_name = BASE_DIRECTORY + database + "/" + database + "_metadata.txt";
+        new File(meta_data_file_name).createNewFile();
+
+        FileWriter fw_global = new FileWriter(BASE_DIRECTORY + GLOBAL_METADATA_FILE, true);
+        FileWriter fw_local = new FileWriter(BASE_DIRECTORY + LOCAL_METADATA_FILE, true);
+
+        fw_global.write(database + "|" + VM_INSTANCE + "\n");
+        fw_local.write(database + "|\n");
+
+        fw_global.close();
+        fw_local.close();
+    }
+
+    public static void executeCreateTable(String query) throws IOException {
+
+        int firstParenthesis=0, lastParenthesis=0;
+        for (int index=0; index<query.length(); index++) {
+            if (Character.compare(query.toCharArray()[index], '(') == 0) {
+                firstParenthesis = index;
+                break;
+            }
+        }
+        for (int index=query.length()-1; index>=0; index--) {
+            if (Character.compare(query.toCharArray()[index], ')') == 0) {
+                lastParenthesis = index;
+                break;
+            }
+        }
+
+        TABLE_NAME = query.substring(0, firstParenthesis).trim().split("\\s+")[2];
+
+
+        // Getting column names and data types
+        List<String> column_names = new ArrayList<>();
+        List<String> column_types = new ArrayList<>();
+        String primary_key=null;
+        String foreign_key=null, fk_details=null;
+
+
+        String[] column_parts = query.substring(firstParenthesis+1,lastParenthesis).trim().split(",");
+        for (String s : column_parts) {
+            if (!(s.contains("primary key") || s.contains("foreign key"))) {
+
+                String[] s_parts = s.trim().split("\\s+");
+                column_names.add(s_parts[0]);
+                column_types.add(s_parts[1]);
+            } else if (s.contains("primary key")) {
+                primary_key = s.substring(s.indexOf("(") + "(".length());
+                primary_key = primary_key.substring(0, primary_key.indexOf(")")).trim();
+            } else if (s.contains("foreign key")) {
+                fk_details = "FOREIGN_KEY ";
+
+                foreign_key = s.substring(s.indexOf("(") + "(".length());
+                foreign_key = foreign_key.substring(0, foreign_key.indexOf(")")).trim();
+
+                fk_details += s.substring(s.indexOf("references"));
+
+
+            }
+        }
+
+        String metadata_header = "";
+        String header = "";
+        for (int i=0; i<column_names.size(); i++) {
+            header += column_names.get(i) + "|";
+
+            if (column_names.get(i).equals(primary_key)) {
+                metadata_header += column_names.get(i) + " " + column_types.get(i) + " PRIMARY_KEY,";
+            } else if (column_names.get(i).equals(foreign_key)) {
+                metadata_header += column_names.get(i) + " " + column_types.get(i) + " " + fk_details + ",";
+            } else {
+                metadata_header += column_names.get(i) + " " + column_types.get(i) + ",";
+            }
+        }
+        metadata_header = metadata_header.substring(0, metadata_header.length()-1);
+        header = header.substring(0, header.length()-1) + "\n";
+
+
+        // add to local metadata files
+        String meta_data_file_name = BASE_DIRECTORY + DATABASE + "/" + DATABASE + "_metadata.txt";
+        FileWriter fw_local = new FileWriter(meta_data_file_name, true);
+        fw_local.write(TABLE_NAME + "|" + metadata_header + "\n");
+        fw_local.close();
+
+
+        // add table name to Local_Meta_Data.txt file
+        List<String> data = new ArrayList<String>();
+        String file_path = BASE_DIRECTORY + LOCAL_METADATA_FILE;
+
+        File metadata = new File(file_path);
+        Scanner reader = new Scanner(metadata);
+        while(reader.hasNextLine()) {
+            String line = reader.nextLine();
+            String[] line_parts = line.split("\\|");
+
+            if (line_parts[0].equals(DATABASE)) {
+                if (line_parts.length > 1) {
+                    line = line.substring(0, line.length()-1);
+                    line += "," + TABLE_NAME + "\n";
+                } else {
+                    line = DATABASE + "|" + TABLE_NAME + "\n";
+                }
+            } else {
+                line += "\n";
+            }
+            data.add(line);
+        }
+
+        FileWriter md = new FileWriter(file_path, false);
+        for (String d : data) {
+            md.write(d);
+        }
+        md.close();
+
+
+        // create table file
+        String table_filepath = BASE_DIRECTORY + DATABASE + "/" + TABLE_NAME + ".txt";
+        File table = new File(table_filepath);
+        table.createNewFile();
+        FileWriter t = new FileWriter(table_filepath);
+        t.write(header);
+        t.close();
+    }
+
+    public static void executeCreate(String query) throws IOException {
+
+        String[] query_parts = query.split("\\s+");
+        String createWhat = query_parts[1];
+
+        if (createWhat.equals("database")) {
+            executeCreateDatabase(query);
+        } else if (createWhat.equals("table")) {
+            executeCreateTable(query);
+        } else {
+            System.out.println("Invalid!");
+        }
     }
 
     public static void executeUse(String query) {
         String[] queryParts = query.split("\\s+");
         String database = queryParts[1];
 
-//        DATABASE = database;
+        DATABASE = database;
     }
 
-    public static void executeInsert() {}
+    public static void executeInsert(String query) throws IOException {
+        // get table_name
+        String[] query_parts = query.split("\\s+");
+        TABLE_NAME = query_parts[2];
 
-    public static void executeSelect() {}
+        String file_path = BASE_DIRECTORY + DATABASE + "/" + TABLE_NAME + ".txt";
 
-    public static void executeUpdate() {}
+        String value_str = query.substring(query.indexOf("values") + "values".length()).trim();
+        value_str = value_str.substring(1, value_str.length()-1).trim();
 
-    public static void executeDelete() {}
+        String[] values = value_str.split(",");
+        String data = "";
 
-//    public static boolean executeQuery(String query) {
-//        String formatted_query = removeExtraSpaces(query);
-//        String queryType = formatted_query.split("\\s")[0];
-//
-//        switch (queryType) {
-//            case "create":
-////                System.out.println("CREATE Query");
-//                return executeCreate(formatted_query);
-//            break;
-//            case "use":
-////                System.out.println("USE Query");
-//                executeUse(formatted_query);
-//                break;
-//            case "insert":
-////                System.out.println("INSERT Query");
-//                executeInsert(formatted_query);
-//                break;
-//            case "select":
-////                System.out.println("SELECT Query");
-//                executeSelect(formatted_query);
-//                break;
-//            case "update":
-////                System.out.println("UPDATE Query");
-//                executeUpdate(formatted_query);
-//                break;
-//            case "delete":
-////                System.out.println("DELETE Query");
-//                executeDelete(formatted_query);
-//                break;
-//            default:
-//                System.out.println("Invalid Query Type! - " + queryType.toUpperCase());
-//        }
-//    }
+        for (String value : values) {
+            value = value.trim();
+            if (value.contains("'")) {
+                value = value.substring(1,value.length()-1);
+            }
+
+            data += value + "|";
+        }
+        data = data.substring(0, data.length()-1).trim() + "\n";
+
+        FileWriter fw = new FileWriter(file_path, true);
+        fw.write(data);
+        fw.close();
+
+
+    }
+
+    public static void executeSelect(String query) throws FileNotFoundException {
+
+        String[] query_parts = query.trim().split("\\s+");
+
+        List<String> data = new ArrayList<String>();
+        List<String> column_names = new ArrayList<String>();
+        String q = query;
+
+        // get table name
+        if (query.contains("where")) {
+            q = q.substring(0, q.indexOf("where")-1);
+            int size = q.split("\\s+").length;
+            TABLE_NAME = q.split("\\s+")[q.split("\\s+").length-1];
+        } else {
+            int size = q.split("\\s+").length;
+            TABLE_NAME = q.split("\\s+")[q.split("\\s+").length-1];
+        }
+
+        String file_path = BASE_DIRECTORY + DATABASE + "/" + TABLE_NAME + ".txt";
+
+        // get column names
+        String header=null;
+        File f = new File(file_path);
+        Scanner sc = new Scanner(f);
+        while (sc.hasNextLine()) {
+            header = sc.nextLine();
+            break;
+        }
+        String[] column_n = header.split("\\|");
+        for (String c : column_n) {
+            column_names.add(c);
+        }
+
+        // Check for * or column names
+        if (query_parts[1].equals("*")) {
+            if (query.contains("where")) {
+                String column_name = query.substring(query.indexOf("where ") + "where ".length());
+                column_name = column_name.substring(0, column_name.indexOf("=")).trim();
+
+                String value = query.substring(query.indexOf("=") + "=".length()).trim();
+
+                int column_number=0;
+                for (int j=0; j<column_names.size(); j++) {
+                    if (column_names.get(j).equals(column_name)) {
+                        column_number = j;
+                        break;
+                    }
+                }
+
+                data.add(header);
+                File file = new File(file_path);
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNextLine()) {
+                    String d = scanner.nextLine();
+                    String[] d_parts = d.split("\\|");
+                    if (d_parts[column_number].equals(value)) {
+                        data.add(d);
+                    }
+                }
+                scanner.close();
+
+            } else {
+                // where is not included
+                // All data
+                File file = new File(file_path);
+                Scanner reader = new Scanner(file);
+                while (reader.hasNextLine()) {
+                    String d = reader.nextLine();
+                    data.add(d);
+                }
+                reader.close();
+            }
+        } else {
+            // Column names
+
+            if (query.contains("where")) {
+                String column_name = query.substring(query.indexOf("where ") + "where ".length());
+                column_name = column_name.substring(0, column_name.indexOf("=")).trim();
+
+                String value = query.substring(query.indexOf("=") + "=".length()).trim();
+
+                // get require column names and number
+                List<Integer> column_numbers = new ArrayList<Integer>();
+                String c_names = query.substring(query.indexOf("select ") + "select".length(), query.indexOf("from"));
+
+
+                String[] c_names_part = c_names.split(",");
+                header = c_names.trim().replaceAll("\\s+", "").replaceAll(",", "|");
+                data.add(header);
+
+
+                // get column numbers
+                for (String cn : c_names_part) {
+                    for (int k=0; k<column_names.size(); k++) {
+                        if (cn.trim().equals(column_names.get(k).trim())) {
+                            column_numbers.add(k);
+                        }
+                    }
+                }
+
+                int column_number=0;
+                for (int j=0; j<column_names.size(); j++) {
+                    if (column_names.get(j).equals(column_name)) {
+                        column_number = j;
+                        break;
+                    }
+                }
+
+                File file = new File(file_path);
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNextLine()) {
+                    String d = scanner.nextLine();
+                    String[] d_parts = d.split("\\|");
+                    if (d_parts[column_number].equals(value)) {
+                        String v = "";
+                        for (int number : column_numbers) {
+                            v += d_parts[number] + "|";
+                        }
+                        v = v.substring(0, v.length()-1);
+                        data.add(v);
+                    }
+                }
+                scanner.close();
+
+            } else {
+                // where is not included
+
+                // get require column names and number
+                List<Integer> column_numbers = new ArrayList<Integer>();
+                String c_names = query.substring(query.indexOf("select ") + "select".length(), query.indexOf("from"));
+
+                String[] c_names_part = c_names.split(",");
+                header = c_names.trim().replaceAll("\\s+", "").replaceAll(",", "|");
+//                data.add(header);
+
+                // get column numbers
+                for (String cn : c_names_part) {
+                    for (int k=0; k<column_names.size(); k++) {
+                        if (cn.trim().equals(column_names.get(k).trim())) {
+                            column_numbers.add(k);
+                        }
+                    }
+                }
+
+                File file = new File(file_path);
+                Scanner scanner = new Scanner(file);
+                while (scanner.hasNextLine()) {
+                    String d = scanner.nextLine();
+                    String[] d_parts = d.split("\\|");
+
+                    String v = "";
+                    for (int number : column_numbers) {
+                        v += d_parts[number] + "|";
+                    }
+                    v = v.substring(0, v.length()-1);
+                    data.add(v);
+                }
+                scanner.close();
+            }
+
+        }
+        System.out.println(data);
+    }
+
+    public static void executeUpdate(String query) {}
+
+    public static void executeDelete(String query) {}
+
+    public static void executeQuery(String query) throws IOException {
+        query = formatQuery(query.toLowerCase().trim());
+
+        String queryType = query.split("\\s+")[0];
+
+
+        switch (queryType) {
+            case "create":
+//                System.out.println("CREATE Query");
+                executeCreate(query);
+                break;
+            case "use":
+//                System.out.println("USE Query");
+                executeUse(query);
+                break;
+            case "insert":
+//                System.out.println("INSERT Query");
+                executeInsert(query);
+                break;
+            case "select":
+//                System.out.println("SELECT Query");
+                executeSelect(query);
+                break;
+            case "update":
+//                System.out.println("UPDATE Query");
+                executeUpdate(query);
+                break;
+            case "delete":
+//                System.out.println("DELETE Query");
+                executeDelete(query);
+                break;
+            default:
+                System.out.println("INVALID QUERY! - " + queryType.toUpperCase());
+        }
+    }
 }
--- src/main/java/queryimplementation/QueryImplementation.java
+++ src/main/java/queryimplementation/QueryImplementation.java
@@ -1,13 +1,7 @@
 package queryimplementation;
 
-import javax.xml.crypto.Data;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import static queryimplementation.ParseQuery.*;
 import static queryimplementation.ExecuteQuery.*;
@@ -17,110 +11,58 @@ public class QueryImplementation {
     public static String DATABASE;
     public static String TABLE_NAME;
 
+    public static String VM_INSTANCE = "VM1";
+
     public static String BASE_DIRECTORY = "VM/";
-    public static String LOCAL_METADATA_FILE = "Local Metadata.txt";
-    public static String GLOBAL_METADATA_FILE = "Global Metadata.txt";
+    public static String LOCAL_METADATA_FILE = "Local_Meta_Data.txt";
+    public static String GLOBAL_METADATA_FILE = "Global_Data_Dictionary.txt";
 
     public static List<String> DATABASES = new ArrayList<>();
     public static List<String> LOCAL_DATABASES = new ArrayList<>();
     public static List<String> GLOBAL_DATABASES = new ArrayList<>();
 
 
-
-//    static String REGEX_FOR_DATABASE_NAME = "[0-9a-zA-Z$_]+";
-
-    // Removes extra spaces at the beginning, end and in-between the words.
-    // Also converts the query to lower case.
-    public static String removeExtraSpaces(String query) {
-        return query.trim().replaceAll("\\s+", " ").toLowerCase();
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
     public static void main(String[] args) throws IOException {
-//        checkRootDirectory();
-//        getDatabase();
 
-//        if (parseQuery(query)) {
-//
-//        }
-//        executeQuery(query);
 
-//        String query = "USE teachers;";
-//        System.out.println("before + " + DATABASE);
+        String[] queries = {
+                "CREATE DATABASE students;",
+                "CREATE DATABASE teachers;",
 
-//        System.out.println(formatQuery("create database    sdasda   ;"));
+                "USE students;",
 
-//        String primary_key_string = Pattern.compile().matcher("create table Orders(OrderID int,OrderNumber int,PersonID int,primary key (OrderID),foreign key (PersonID) references Persons(PersonID));").group(1);
+                "CREATE TABLE student_details (ID int, Name varchar(25), phone varchar(10), PRIMARY KEY (ID), FOREIGN KEY (name) REFERENCES teachers(name));",
 
-//        Pattern pattern = Pattern.compile("primary\\skey\\s*\\(\\s*[0-9a-zA-Z_]+\\s*\\)\\s*");
-//        Matcher matcher = pattern.matcher("create table Orders(OrderID int,OrderNumber int,PersonID int,primary key (OrderID),foreign key (PersonID) references Persons(PersonID));");
+                "INSERT INTO student_details VALUES (1, 'kalpit', '1111111111');",
+                "INSERT INTO student_details VALUES (2, 'vishnu', '2222222222');",
+                "INSERT INTO student_details VALUES (3, 'kavya', '3333333333');",
+                "INSERT INTO student_details VALUES (4, 'sharad', '4444444444');",
+                "INSERT INTO student_details VALUES (5, 'kishan', '5555555555');",
 
-//        Pattern pattern = Pattern.compile("[0-9a-zA-Z_]+");
-//        Matcher matcher = pattern.matcher("create table Orders(OrderID int,OrderNumber int,PersonID int,primary key (OrderID),foreign key (PersonID) references Persons(PersonID));");
-//
-//
-//        if (matcher.find()) {
-//            System.out.println("Hi");
-//            System.out.println(matcher.group(1));
-//            System.out.println(matcher.group(2));
-////            Assert.assertEquals("25-09-1984", matcher.group());
-//        }
+                "CREATE TABLE student_grades (ID int, sub1 float, sub2 float);",
 
-        String query = "create table Orders(OrderID int,OrderNumber int,PersonID int,primary key (OrderID),foreign key (PersonID) references Persons(PersonID));";
-//        int first=0, last=0;
-//        int start_index = query.indexOf("primary key") + "primary key".length();
-//        for (int i=start_index; i<query.length(); i++) {
-//            if (Character.compare(query.toCharArray()[i], '(') == 0) {
-//                first = i;
-//            }
-//            if (Character.compare(query.toCharArray()[i], ')') == 0) {
-//                last = i;
-//                break;
-//            }
-//        }
-//
-//        String pk_column = query.substring(first+1, last).trim();
-//        System.out.println(pk_column);
+                "INSERT INTO student_grades VALUES (1, 85, 92);",
+                "INSERT INTO student_grades VALUES (2, 87, 92);",
+                "INSERT INTO student_grades VALUES (3, 80, 99);",
+                "INSERT INTO student_grades VALUES (4, 76, 91);",
+                "INSERT INTO student_grades VALUES (5, 100, 82);",
 
-        Set<String> column_names = new HashSet<String>();
-        int column_count = 0;
+                "SELECT * FROM student_grades where sub2=92;",
+                "SELECT name, phone FROM student_details;",
 
-        int firstParenthesis=0, lastParenthesis=0;
-        for (int index=0; index<query.length(); index++) {
-            if (Character.compare(query.toCharArray()[index], '(') == 0) {
-                firstParenthesis = index;
-                break;
-            }
-        }
-        for (int index=query.length()-1; index>=0; index--) {
-            if (Character.compare(query.toCharArray()[index], ')') == 0) {
-                lastParenthesis = index;
-                break;
+                "USE teachers;",
+
+                "CREATE TABLE staff (t_id int, t_name varchar(25));"
+        };
+
+        for (String q : queries) {
+            if(parseQuery(q)) {
+                executeQuery(q);
+            } else {
+                System.out.println("INVALID QUERY!");
             }
         }
 
-        String[] column_parts = query.substring(firstParenthesis+1,lastParenthesis).trim().split(",");
-        for (String s : column_parts) {
-            if (!(s.contains("primary key") || s.contains("foreign key"))) {
-                column_count += 1;
-
-                String[] s_parts = s.trim().split("\\s+");
-                column_names.add(s_parts[0]);
-            }
-        }
-        System.out.println(column_count);
-        System.out.println(column_names);
 
     }
 }

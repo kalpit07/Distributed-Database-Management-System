84c4956e8f8f4ab0c345b048f4939fe730abdfa7
sharad <sh244392.dal.ca>
2022-04-10 01:06:33 -0300

    pom file changes

--- src/main/java/Connection/SSHConnect.java
+++ src/main/java/Connection/SSHConnect.java
@@ -7,7 +7,7 @@ import java.io.ByteArrayOutputStream;
 public class SSHConnect {
 
     public static void main(String []args) throws Exception {
-        listFolderStructure("gcpKey", "", "34.122.248.27", 22, "java -jar JARGenerator.jar hello");
+        listFolderStructure("gcpKey", "123qwe", "34.136.116.68", 22, "java -jar D2_DB.jar");
         //listFolderStructure("gcpKey", "", "34.136.127.213", 22, "dir");
     }
 
